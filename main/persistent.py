1775072c90fda5d01c99edc94948e383767b3102
antonysavache <89783847+antonysavache@users.noreply.github.com>
2024-11-07 13:33:17 +0200

    done

--- main/persistent.py
+++ main/persistent.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from models import AddressBook
 
 def save_address_book(book: AddressBook, filename="address_book.pkl"):
-    """Save the address book to a file using pickle serialization."""
     try:
         with open(filename, "wb") as file:
             pickle.dump(book, file)
@@ -11,7 +10,6 @@ def save_address_book(book: AddressBook, filename="address_book.pkl"):
         print(f"Error saving address book: {e}")
 
 def load_address_book(filename="address_book.pkl") -> AddressBook:
-    """Load the address book from a file using pickle deserialization."""
     if Path(filename).exists():
         try:
             with open(filename, "rb") as file:
